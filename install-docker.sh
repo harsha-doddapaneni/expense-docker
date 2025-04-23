87dc1237e31174e309c890e52d365753c2acb680
harsha-doddapaneni <harshadoddapaneni1@gmail.com>
2025-04-23 13:56:26 +0530

    expense-docker

--- install-docker.sh
+++ install-docker.sh
@@ -7,7 +7,7 @@ N="\e[0m"
 dnf -y install dnf-plugins-core
 dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
 dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-systemctl enable --now docker
+# systemctl enable --now docker
 systemctl start docker
 systemctl status docker
 usermod -aG docker ec2-user

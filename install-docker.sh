a1862b1520b427b4efdefa369dd2a51dbc71e109
harsha-doddapaneni <harshadoddapaneni1@gmail.com>
2025-04-23 14:12:30 +0530

    expense-docker

--- install-docker.sh
+++ install-docker.sh
@@ -7,8 +7,8 @@ N="\e[0m"
 dnf -y install dnf-plugins-core
 dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
 dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-# systemctl enable --now docker
 systemctl start docker
+systemctl enable --now docker
 systemctl status docker
 usermod -aG docker ec2-user
 echo -e "$R Logout and Login again $N"
